3d1a45f8f674d9dbf57bf52add857fbd7096d32c
Cole Flournoy <flournoy.cole@gmail.com>
2020-12-15 16:18:44 -0500

    commit tree 43ad6844953d504c7c636964e97b019cfb155d18

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,47 @@
-# code here!
+## Change folder locations after this lab
+require "pry"
+
+class School
+    attr_accessor :name, :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        if @roster == {}
+            @roster[grade] = []
+            @roster[grade] <<= name
+            @roster
+        else   
+            @roster.clone.each do |key, value|
+                 if key == grade  
+                    value <<= name 
+                    @roster[grade] = value
+                else
+                    @roster[grade] = []
+                    @roster[grade] <<= name 
+                    @roster
+                end
+            end
+            @roster
+        end
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @roster.each do |key, value|
+            @roster[key] = value.sort
+        end
+        @roster
+    end
+
+end
+
+school = School.new("Bayside High School") 
+school.add_student("It's Ya Boi", 100)
+school.roster
